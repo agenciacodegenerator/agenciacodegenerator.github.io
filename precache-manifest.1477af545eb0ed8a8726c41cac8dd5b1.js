1fd638b4f1fadc07bed6d7a14eefc070fde9a58d
Juliano Terres <juliano@julianoterres.com.br>
2020-05-26 13:03:18 -0300

    v1.0.0

---
+++ precache-manifest.1477af545eb0ed8a8726c41cac8dd5b1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c3760879a1a82153f582a04015614a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "051320a0b80c85c527ff",
+    "url": "/static/css/main.41745985.chunk.css"
+  },
+  {
+    "revision": "b73edbefadf0b61e3ed8",
+    "url": "/static/js/2.5a77f83a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a77f83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051320a0b80c85c527ff",
+    "url": "/static/js/main.f37af5e0.chunk.js"
+  },
+  {
+    "revision": "cf06e91897761940f5d3",
+    "url": "/static/js/runtime-main.d3cf82e2.js"
+  },
+  {
+    "revision": "de21d79f65cb30c758461909e7e04476",
+    "url": "/static/media/beira-rio.de21d79f.jpg"
+  },
+  {
+    "revision": "6b20357fcc379afecbbe547bcdd17965",
+    "url": "/static/media/c&a.6b20357f.jpg"
+  },
+  {
+    "revision": "a818deda2ea0acca498f7cabc10a4fad",
+    "url": "/static/media/globoplay.a818deda.jpg"
+  },
+  {
+    "revision": "f4dff5383bd3461d8658475005622e7e",
+    "url": "/static/media/goodcard.f4dff538.jpg"
+  },
+  {
+    "revision": "5c797695c893afc677c654366a70cd68",
+    "url": "/static/media/ipanema.5c797695.jpg"
+  },
+  {
+    "revision": "d33bdd91dc084e5ec5e9979d9eb69c89",
+    "url": "/static/media/logo.d33bdd91.png"
+  },
+  {
+    "revision": "5c547d12968e6059c1f6326924f8a2b3",
+    "url": "/static/media/melissa.5c547d12.jpg"
+  },
+  {
+    "revision": "46091deb0ec3dca0c1c1e782692cf1ea",
+    "url": "/static/media/pompeia.46091deb.jpg"
+  },
+  {
+    "revision": "fce4d9f8962364a6d90c5cc2e7a5d9b0",
+    "url": "/static/media/rider.fce4d9f8.jpg"
+  },
+  {
+    "revision": "0cc11400fa7e5c128d215a023033bb44",
+    "url": "/static/media/swaap.0cc11400.jpg"
+  },
+  {
+    "revision": "cb4b9cd15c332984422f2586887925da",
+    "url": "/static/media/taqi.cb4b9cd1.jpg"
+  },
+  {
+    "revision": "6ccc91c32584a9740e495843a28e47d1",
+    "url": "/static/media/ticket-log.6ccc91c3.jpg"
+  },
+  {
+    "revision": "550ab1b7a03227806f9bb9759e5e3d2b",
+    "url": "/static/media/unicred.550ab1b7.jpg"
+  }
+]);
